566d64ffd7ed920f1afd76d1d5d4cc3314dc5cb8
Gustavo Miguel <gustavo.biscaro@gmail.com>
2021-03-23 21:41:37 -0300

    added index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+﻿<form method="post" enctype="multipart/form-data">
+
+    Arquivo: <br/>
+    <input type="file" name="arquivo" multiple> <br/><br/>
+
+    <input type="submit" value="Enviar">
+
+</form>
